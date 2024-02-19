829751ecf6b8e0416e19872666e075b4fb2fdcaf
HunterYocum <hunter.p.yocum@gmail.com>
2024-02-19 15:17:38 -0500

    Chapter 6 Demo

---
+++ app/src/main/java/edu/quinnipiac/secretmessage/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.quinnipiac.secretmessage
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.quinnipiac.secretmessage.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
